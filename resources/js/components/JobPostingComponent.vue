0fa2c6cf68a4ee7e33c38e2f04e02c9781dbd437
emborromeo <ericaborromeo06@gmail.com>
2021-01-25 01:10:12 +0800

    changes

--- resources/js/components/JobPostingComponent.vue
+++ resources/js/components/JobPostingComponent.vue
@@ -79,7 +79,7 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="close()">Close</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         <button type="button" class="btn btn-primary" @click="save()">Save changes</button>
                     </div>
                 </div>
